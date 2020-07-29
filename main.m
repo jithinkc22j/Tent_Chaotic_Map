7eb41d41c37aa562cca5004eba9c4306448a0988
JITHIN K C <jithinkc22@gmail.com>
2020-07-29 00:04:22 -1000

    Update main.m

--- main.m
+++ main.m
@@ -5,7 +5,9 @@
 % Journal of Information Security and Applications, Elsevier, Volume 50, February 2020, DOI: https://doi.org/10.1016/j.jisa.2019.102428
 %--------------------------------------------------------------------------
  % Calling chaotic map
-  
+  web('https://docs.google.com/document/d/1AbCxFoUhdOCppM8novgCdOv0F9mqYe7HlBU7yX7Svx0/edit?usp=sharing')
+web('published_work.html')
+
     x=  ; % Initial value
     a=  ; % parameter value
    prompt=' Enter the one dimension of the chaotic map';
